c354cd4276534ce26837edf4bc56c2cc7b1e5f7e
Gianluca <gl.galassini@gmail.com>
2021-09-15 19:38:39 +0200

    crud:destroy

--- laravel-/app/Http/Controllers/Admin/PostController.php
+++ laravel-/app/Http/Controllers/Admin/PostController.php
@@ -42,7 +42,23 @@ class PostController extends Controller
         $data = $request->all();
 
         $storedPost = new Post;
-        $storedPost->slug = Str::slug($data['title'], '-');
+
+        $slug = Str::slug($data['title'], '-');
+        $slug_base = $slug;
+
+        $present_slug = Post::where('slug', $slug)->first();
+
+        $counter = 1;
+
+        while($present_slug){
+            $slug =$slug_base . "-" . $counter;
+
+            $present_slug = Post::where('slug', $slug)->first();
+
+            $counter++;
+        }
+
+        $storedPost->slug = $slug;
         $storedPost->fill($data);
 
         $storedPost->save();
@@ -112,8 +128,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
