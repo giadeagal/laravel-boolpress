d3463c5070a21122f0887e307bc376896ef49d3c
Gianluca <gl.galassini@gmail.com>
2021-09-15 18:40:43 +0200

    slug

--- laravel-/resources/views/layouts/app.blade.php
+++ laravel-/resources/views/layouts/app.blade.php
@@ -80,9 +80,9 @@
             <div class="row">
                 <div class="col-2">
                   <div class="list-group" id="list-tab" role="tablist">
-                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Dashboard</a>
-                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Posts</a>
-                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Users</a>
+                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="{{ route('admin.posts.index')}}" role="tab" aria-controls="list-home">Dashboard</a>
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="{{ route('admin.posts.index')}}" role="tab" aria-controls="list-profile">Posts</a>
+                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="{{ route('admin.posts.create')}}" role="tab" aria-controls="list-messages">New post</a>
                     <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Categories</a>
                     <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Tags</a>
                   </div>
